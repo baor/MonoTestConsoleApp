fd6b3077d9f29ee821423ee5b1c492a3971380e2
Igor Rybak <irybak@travix.com>
2018-07-23 18:22:30 +0200

    add simple project

---
+++ MonoTestConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+using Shouldly;
+using System;
+using System.Net;
+
+namespace MonoTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://myaccount-authentication.travix.com");
+            var request = new RestRequest("/cheaptickets/account/login", Method.POST);
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddBody("{\"password\":\"aaaaa\",\"emailAddress\":\"a@a.aa\",\"generateRememberMeToken\":false,\"AffiliateCode\":\"CheapticketsNL\",\"BrandName\":\"cheaptickets\",\"CountryCode\":\"NL\",\"LanguageCode\":\"nl\"}");
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine($"Response code: {response.StatusCode}");
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest, $"Expected status code: {HttpStatusCode.BadRequest}");
+        }
+    }
+}
